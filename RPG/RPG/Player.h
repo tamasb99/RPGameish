9907737e2399edc73d620aea9eb7c853d4369d39
tamasb99 <tamasb99@users.noreply.github.com>
2020-04-28 09:26:33 +0300

    Kezdes

---
+++ RPG/RPG/Player.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define WIN32_LEAN_AND_MEAN
+
+#include <stdlib.h>
+#include <stdio.h>
+#include 
+#endif // !PLAYER_h
